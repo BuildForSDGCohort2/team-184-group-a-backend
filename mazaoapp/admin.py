ecfada373ad7633a0f089ccbb87d6e0809802ba1
Charles <Charleskyalo006@gmail.com>
2020-09-16 14:34:14 +0300

    updated project folders

--- mazaoapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Farmer
-
-# Register your models here.
-admin.site.register(Farmer)
--- mazaoapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MazaoappConfig(AppConfig):
-    name = 'mazaoapp'
